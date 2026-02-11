3c7ad8670ec9dd105927521522f08dc07ae9e262
Jewelzzz <jewelalanahicks@gmail.com>
2026-02-11 13:38:35 +0000

    lab 2 - BracketChecker implemented

--- src/project20280/stacksqueues/BracketChecker.java
+++ src/project20280/stacksqueues/BracketChecker.java
@@ -1,17 +1,75 @@
 package project20280.stacksqueues;
 
 class BracketChecker {
-    private final String input;
+    private String input;
 
-    public BracketChecker(String in) {
+    public BracketChecker(String in)
+    {
         input = in;
     }
 
-    public void check() {
-        // TODO
+    private boolean isLeft(char c)
+    {
+        return c == '(' || c == '[' || c == '{';
     }
 
-    public static void main(String[] args) {
+    private boolean isRight(char c)
+    {
+        return c == ')' || c == ']' || c == '}';
+    }
+
+    private boolean matches(char left, char right)
+    {
+        return (left == '(' && right == ')')
+                || (left == '[' && right == ']')
+                || (left == '{' && right == '}');
+    }
+
+
+    public void check() {
+        // TODO
+        ArrayStack<Character> stack = new ArrayStack<>();
+
+        for (int i = 0; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+
+            if (isLeft(c))
+            {
+                stack.push(c);
+            }
+
+            else if (isRight(c))
+            {
+                if (stack.isEmpty())
+                {
+                    System.out.println("error at index " + i + ": missing left delimiter for '" + c + "'");
+                    return;
+                }
+
+                char left = stack.pop();
+
+                if (!matches(left, c))
+                {
+                    System.out.println("error at index " + i + ": '" + c + "' does not match '" + left + "'");
+                    return;
+                }
+            }
+        }
+
+        if (!stack.isEmpty())
+        {
+            System.out.println("error: missing delimiter; stack not empty at end.");
+        }
+
+        else
+        {
+            System.out.println("correct: delimiters are balanced");
+        }
+    }
+
+    public static void main(String[] args)
+    {
         String[] inputs = {
                 "[]]()()", // not correct
                 "c[d]", // correct\n" +
@@ -21,10 +79,12 @@ class BracketChecker {
                 "a{b(c) ", // // not correct; Nothing matches opening {
         };
 
-        for (String input : inputs) {
+        for (String input : inputs)
+        {
             BracketChecker checker = new BracketChecker(input);
             System.out.println("checking: " + input);
             checker.check();
+            System.out.println("\n");
         }
     }
 }
